06829bc2d6b57efeaeb62bc84067b9ea69a3e56d
guney <guneyyilmaz2707@gmail.com>
2023-11-25 20:43:03 +0300

    4.0.1-BETA Kotlin Update

--- src/main/java/net/guneyilmaz0/mongos/MongoSObject.java
+++
@@ -1,9 +0,0 @@
-package net.guneyilmaz0.mongos;
-
-import com.google.gson.Gson;
-
-public abstract class MongoSObject {
-    public String toString(){
-        return new Gson().toJson(this);
-    }
-}

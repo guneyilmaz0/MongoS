19bb306260a9a1a4ab2fa7bf82fa6ce2974f9349
guney <guneyyilmaz2707@gmail.com>
2024-12-18 20:18:25 +0300

    .

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
     implementation("org.mongodb:mongodb-driver-sync:5.2.0")
-    implementation("com.go  ogle.code.gson:gson:2.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:2.0.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.20.0")
